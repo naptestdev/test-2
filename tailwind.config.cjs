129877099ee9bd0c021fd435fbc9a1717bf2e8c7
napthedev <phongna.dev@gmail.com>
2023-01-10 22:07:06 +0700

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        red: "#FE5F5F",
+        third: "#FFC9AA",
+        first: "#FFCA28",
+        second: "#68FFF6",
+      },
+    },
+  },
+  plugins: [],
+};
